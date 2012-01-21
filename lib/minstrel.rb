132159fa6e1005a93fc077eb298e99a10e289284
Jordan Sissel <jls@semicomplete.com>
2012-01-21 13:34:46 -0800

    - commence refactor to use Kernel#set_trace_func instead of wrapping   methods.

--- lib/minstrel.rb
+++ lib/minstrel.rb
@@ -16,267 +16,101 @@
 # RUBY_INSTRUMENT=comma_separated_classnames ruby -rminstrel ./your/program.rb
 #
 
-require "set"
+TraceEvent = Struct.new(:action, :file, :line, :method, :binding, :class, :args)
 
 module Minstrel; class Instrument
   attr_accessor :counter
 
-  class << self
-    @@deferred_wraps = {}
-    @@deferred_method_wraps = {}
-    @@wrapped = Set.new
-  end
+  public
+  def self.singleton
+    @minstrel ||= Minstrel::Instrument.new
+  end # def self.singleton
 
-  # Put methods we must not be wrapping here.
-  DONOTWRAP = {
-    "Minstrel::Instrument" => Minstrel::Instrument.instance_methods.collect { |m| m.to_sym },
-    "Object" => [ :to_sym, :respond_to?, :send, :java_send, :method, :java_method,
-                  :ancestors, :inspect, :to_s, :instance_eval, :instance_exec,
-                  :class_eval, :class_exec, :module_eval, :module_exec],
-  }
+  public
+  def initialize
+    @observe = []
+  end # def initialize
 
-  # Wrap a class's instance methods with your block.
-  # The block will be called with 4 arguments, and called
-  # before and after the original method.
-  # Arguments:
-  #   * point - the point (symbol, :entry or :exit) of call,
-  #   * this - the object instance in scope (use 'this.class' for the class)
-  #   * method - the method (symbol) being called
-  #   * *args - the arguments (array) passed to this method.
-  def wrap(klass, method_to_wrap=nil, &block)
-    return true if @@wrapped.include?(klass)
-    instrumenter = self # save 'self' for scoping below
-    @@wrapped << klass
-
-    ancestors = klass.ancestors.collect {|k| k.to_s } 
-    if ancestors.include?("Exception")
-      return true
-    end
-    puts "Wrapping #{klass.class} #{klass}" if $DEBUG
-
-    # Wrap class instance methods (like File#read)
-    klass.instance_methods.each do |method|
-      next if !method_to_wrap.nil? and method != method_to_wrap
-
-      method = method.to_sym
-
-      # If we shouldn't wrap a certain class method, skip it.
-      skip = false
-      (ancestors & DONOTWRAP.keys).each do |key|
-        if DONOTWRAP[key].include?(method)
-          skip = true 
-          break
-        end
-      end
-      if skip
-        #puts "Skipping #{klass}##{method} (do not wrap)"
-        next
-      end
-
-      klass.class_eval do
-        orig_method = "#{method}_original(wrapped)".to_sym
-        orig_method_proc = klass.instance_method(method)
-        alias_method orig_method, method
-        #block.call(:wrap, klass, method)
-        puts "Wrapping #{klass.name}##{method} (method)" if $DEBUG
-        define_method(method) do |*args, &argblock|
-          exception = false
-          block.call(:enter, self, method, *args)
-          begin
-            # TODO(sissel): Not sure which is better:
-            # * UnboundMethod#bind(self).call(...)
-            # * self.method(orig_method).call(...)
-            val = orig_method_proc.bind(self).call(*args, &argblock)
-            #m = self.method(orig_method)
-            #val = m.call(*args, &argblock)
-          rescue => e
-            exception = e
+  public
+  def observe(thing=nil, &block)
+    if !thing.nil?
+      p :observe => thing
+      # Is thing a class name?
+      @observe << lambda do |event| 
+        if class?(thing)
+          if event.class.to_s == thing
+            block.call(event) 
           end
-          if exception
-            # TODO(sissel): Include the exception
-            block.call(:exit_exception, self, method, *args)
-            raise e if exception
-          else
-            # TODO(sissel): Include the return value
-            block.call(:exit, self, method, *args)
-          end
-          return val
-        end # define_method(method)
-      end # klass.class_eval
-    end # klass.instance_methods.each
-
-    # Wrap class methods (like File.open)
-    klass.methods.each do |method|
-      next if !method_to_wrap.nil? and method != method_to_wrap
-      method = method.to_sym
-      # If we shouldn't wrap a certain class method, skip it.
-      skip = false
-      ancestors = klass.ancestors.collect {|k| k.to_s} 
-      (ancestors & DONOTWRAP.keys).each do |key|
-        if DONOTWRAP[key].include?(method)
-          skip = true 
-          #break
-        end
-      end
-
-      # Doubly-ensure certain methods are not wrapped.
-      # Some classes like "Timeout" do not have ancestors.
-      if DONOTWRAP["Object"].include?(method)
-        #puts "!! Skipping #{klass}##{method} (do not wrap)"
-        skip = true
-      end
-
-      if skip
-        puts "Skipping #{klass}##{method} (do not wrap, not safe)" if $DEBUG
-        next
-      end
-
-      klass.instance_eval do
-        orig_method = "#{method}_original(classwrapped)".to_sym
-        (class << self; self; end).instance_eval do
-          begin
-            alias_method orig_method, method.to_sym
-          rescue NameError => e
-            # No such method, strange but true.
-            orig_method = self.method(method.to_sym)
-          end
-          method = method.to_sym
-          #puts "Wrapping #{klass.name}.#{method} (classmethod)"
-          define_method(method) do |*args, &argblock|
-            block.call(:class_enter, self, method, *args)
-            exception = false
-            begin
-              if orig_method.is_a?(Symbol)
-                val = send(orig_method, *args, &argblock)
-              else
-                val = orig_method.call(*args, &argblock)
-              end
-            rescue => e
-              exception = e
-            end
-            if exception
-              block.call(:class_exit_exception, self, method, *args)
-              raise e if exception
-            else
-              block.call(:class_exit, self, method, *args)
-            end
-            return val
+        else
+          # assume it's a method?
+          klass, method = thing.split(/[.#]/, 2)
+          if (event.class.to_s == klass and event.method = method.to_sym)
+            block.call(event)
           end
         end
-        #block.call(:class_wrap, self, method, self.method(method))
-      end # klass.instance_eval
-    end # klass.instance_methods.each
-
-    return true
-  end # def wrap
-
-  def wrap_classname(klassname, &block)
-    begin
-      klass = eval(klassname)
-      wrap(klass, &block) 
-      return true
-    rescue NameError => e
-      @@deferred_wraps[klassname] = block
-    end
-    return false
-  end
-
-  def wrap_method(fullname, &block)
-    puts "Want to wrap #{fullname}" if $DEBUG
-    begin
-      klassname, method = fullname.split(/[#.]/, 2)
-      klass = eval(klassname)
-      wrap(klass, method, &block)
-      return true
-    rescue NameError => e
-      @@deferred_method_wraps[fullname] = block
-      return false
-    end
-  end # def wrap_method
-
-  def wrap_all(&block)
-    @@deferred_wraps[:all] = block
-    ObjectSpace.each_object do |obj|
-      next unless obj.is_a?(Class)
-      wrap(obj, &block)
-    end
-  end
-
-  def self.wrap_require
-    Kernel.class_eval do
-      alias_method :old_require, :require
-      def require(*args)
-        return Minstrel::Instrument::instrumented_loader(:require, *args)
       end
-    end
-  end
-
-  def self.wrap_load
-    Kernel.class_eval do
-      alias_method :old_load, :load
-      def load(*args)
-        return Minstrel::Instrument::instrumented_loader(:load, *args)
-      end
-    end
-  end
-
-  def self.instrumented_loader(method, *args)
-    ret = self.send(:"old_#{method}", *args)
-    if @@deferred_wraps.include?(:all)
-      # try to wrap anything new that is not wrapped
-      wrap_all(@@deferred_wraps[:all])
+    elsif block_given?
+      @observe << block
     else
-      # look for deferred class wraps
-      klasses = @@deferred_wraps.keys
-      klasses.each do |klassname|
-        if @@deferred_wraps.include?("ALL")
-          all = true
-        end
-        block = @@deferred_wraps[klassname]
-        instrument = Minstrel::Instrument.new
-        if instrument.wrap_classname(klassname, &block)
-          $stderr.puts "Wrap of #{klassname} successful"
-          @@deferred_wraps.delete(klassname) if !all
-        end
-      end
-
-      klassmethods = @@deferred_method_wraps.keys
-      klassmethods.each do |fullname|
-        block = @@deferred_method_wraps[fullname]
-        instrument = Minstrel::Instrument.new
-        if instrument.wrap_method(fullname, &block)
-          $stderr.puts "Wrap of #{fullname} successful"
-          @@deferred_method_wraps.delete(fullname)
-        end
-      end
+      raise ArgumentError.new("No block given.")
     end
-    return ret
-  end
-end; end # class Minstrel::Instrument
+  end # def observe
 
-Minstrel::Instrument.wrap_require
-Minstrel::Instrument.wrap_load
+  # Is the name here a class?
+  private
+  def class?(name)
+    begin
+      return false if name.include?("#")
+      obj = eval(name)
+      return obj.is_a?(Class)
+    rescue => e
+      false
+    end
+  end # def class?
+
+  # Activate tracing
+  public
+  def enable
+    set_trace_func(Proc.new { |*args| trace_callback(*args) })
+  end # def enable
+
+  # Disable tracing
+  public
+  def disable
+    set_trace_func(nil)
+  end # def disable
+
+  # This method is invoked by the ruby tracer
+  private
+  def trace_callback(action, file, line, method, binding, klass)
+    return unless action == "c-call"
+    event = TraceEvent.new(action, file, line, method, binding, klass)
+    # At the time of "c-call" there's no other local variables other than
+    # the method arguments. Pull the args out of binding
+    #eval('args = local_variables.collect { |v| "(#{v}) #{v.inspect}" }', binding)
+    eval('p :local => local_variables', binding)
+    #event.args = args
+
+    @observe.each { |callback| callback.call(event) }
+  end # def trace_callback
+end; end # class Minstrel::Instrument
 
 # Provide a way to instrument a class using the command line:
 # RUBY_INSTRUMENT=String ruby -rminstrel ./your/program
 if ENV["RUBY_INSTRUMENT"]
   klasses = ENV["RUBY_INSTRUMENT"].split(",")
 
-  callback = proc do |point, this, method, *args|
-    puts "#{point} #{this.class.to_s}##{method}(#{args.inspect}) (thread=#{Thread.current}, self=#{this.inspect})"
+  callback = proc do |event|
+    puts "#{event.action} #{event.class.to_s}##{event.method}(#{event.args.inspect}) (thread=#{Thread.current})"
   end
+
   instrument = Minstrel::Instrument.new 
   if klasses.include?(":all:")
-    instrument.wrap_all(&callback)
+    instrument.observe(&callback)
   else
     klasses.each do |klassname|
-      if klassname =~ /[#.]/ # Someone's asking for a specific method to wrap
-        # This will wrap one method as indicated by: ClassName#method
-        # TODO(sissel): Maybe also allow ModuleName::method
-        instrument.wrap_method(klassname, &callback) 
-      else
-        instrument.wrap_classname(klassname, &callback) 
-      end
+      instrument.observe(klassname, &callback)
     end # klasses.each
   end 
+  instrument.enable
 end # if ENV["RUBY_INSTRUMENT"]
